6375b5665e48fd36b0456e31a60acfaac7f33a9f
Nate Argaw <nateargaw@gmail.com>
2021-12-29 11:47:05 -0500

    Added BloomPass

--- src/script.js
+++ src/script.js
@@ -1,10 +1,15 @@
 import './style.css'
 import * as THREE from 'three'
+import * as dat from 'dat.gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import img from './img/particle.png'
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer'
+import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass'
+import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass'
+import {UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPass'
 const canvas = document.querySelector('.webgl')
 
 class NewScene{
@@ -16,10 +21,12 @@ class NewScene{
         this.scene = new THREE.Scene()
         this.clock = new THREE.Clock()
         this.gltfLoader = new GLTFLoader()
+        this.InitSettings()
         this.Texture()
         this.Model()
         this.InitCamera()
         this.InitRenderer()
+        this.InitPostProcessing()
         this.BoxParticles()
         this.InitLights()
         this.InitControls()
@@ -27,6 +34,20 @@ class NewScene{
         window.addEventListener('resize', () => {
             this.Resize()
         })
+    }
+
+    InitSettings(){
+        this.settings = {
+            progress: 0,
+            bloomStrength: 0.8,
+            bloomRadius: 0.6,
+            bloomThreshold: 0.2
+        }
+        this.gui = new dat.GUI()
+        //this.gui.add(this.settings, 'progress', 0, 1, 0.01)
+        this.gui.add(this.settings, 'bloomStrength', 0, 10, 0.01)
+        this.gui.add(this.settings, 'bloomRadius', 0, 10, 0.01)
+        this.gui.add(this.settings, 'bloomThreshold', 0, 10, 0.01)
     }
 
     Texture(){
@@ -104,12 +125,21 @@ class NewScene{
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
         this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
         this.renderer.setSize(window.innerWidth, window.innerHeight)
-        this.renderer.render(this.scene, this.camera)
+        //this.renderer.render(this.scene, this.camera)
+    }
+
+    InitPostProcessing(){
+        this.renderScene = new RenderPass(this.scene, this.camera)
+        this.bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 0.6, 0.8, 0.05)
+        this.composer = new EffectComposer(this.renderer)
+        this.composer.addPass(this.renderScene)
+        this.composer.addPass(this.bloomPass)
+        
     }
 
     InitCamera(){
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
-        this.camera.position.set(10, 9, 0)
+        this.camera.position.set(10, 9, 20)
         this.scene.add(this.camera)
     }
 
@@ -122,6 +152,7 @@ class NewScene{
         this.controls = new OrbitControls(this.camera, canvas)
         this.controls.enableDamping = true
         this.controls.autoRotate = true
+        this.controls.autoRotateSpeed = 0.5
         this.controls.update()
     }
 
@@ -134,8 +165,14 @@ class NewScene{
     Update(){
         requestAnimationFrame(() => {
             this.elapsedTime = this.clock.getElapsedTime()
-            this.material.uniforms.u_time.value = this.elapsedTime   
-            this.renderer.render(this.scene, this.camera)
+            this.material.uniforms.u_time.value = this.elapsedTime
+            if(this.bloomPass){
+                this.bloomPass.threshold = this.settings.bloomThreshold
+                this.bloomPass.strength = this.settings.bloomStrength
+                this.bloomPass.radius = this.settings.bloomRadius
+                this.composer.render(this.scene, this.camera)
+            }   
+            //this.renderer.render(this.scene, this.camera)
             this.controls.update()
             this.Update()
         })  
